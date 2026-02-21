2fe5b8d90ce29fc0a79944eeafd6c82e6d5e1098
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2026-02-20 22:11:27 -0800

    Add financial tips reducer to store configuration

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import transactionsReducer from './transactionsSlice';
 import budgetReducer from './budgetSlice';
 import categoriesReducer from './categoriesSlice';
+import financialTipsReducer from './financialTipsSlice';
 
 export const store = configureStore({
   reducer: {
     transactions: transactionsReducer,
     budget: budgetReducer,
     categories: categoriesReducer,
+    financialTips: financialTipsReducer,
   },
 });
