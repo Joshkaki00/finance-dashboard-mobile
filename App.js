9a9d57953157b157bfabf6bffdc8f814666a3ba5
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2026-02-20 22:09:29 -0800

    Refactor App component to load persisted transactions and budgets on launch

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useEffect } from 'react';
+import { Provider, useDispatch } from 'react-redux';
 import { store } from './src/store/store';
 import AppNavigator from './src/navigation/AppNavigator';
+import { loadPersistedTransactions } from './src/store/transactionsSlice';
+import { loadPersistedBudgets } from './src/store/budgetSlice';
+
+function AppContent() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Load persisted data on app launch
+    dispatch(loadPersistedTransactions());
+    dispatch(loadPersistedBudgets());
+  }, [dispatch]);
+
+  return <AppNavigator />;
+}
 
 export default function App() {
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <AppNavigator />
-        <StatusBar style="auto" />
-      </SafeAreaProvider>
+      <AppContent />
     </Provider>
   );
 }
